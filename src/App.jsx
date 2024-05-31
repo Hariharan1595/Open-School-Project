351a1f53d4887f645a93615fe063b9dfca6a24c3
GR <rajga@GR.com>
2024-05-31 18:15:29 +0530

    added About.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Academics from "./pages/Academics";
 
 function App() {
   return (
-    <>
-    <Navbar />
-    <Home />
-    <Footer />
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/academics" element={<Academics />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
